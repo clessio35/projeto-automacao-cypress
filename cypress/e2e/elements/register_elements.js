6639292a93dc41870d8cf366e5e68909b5676f6a
Clessio <clessio.silva1989@gmail.com>
2025-03-31 09:08:26 -0300

    created methods in registerPage class

--- cypress/e2e/elements/register_elements.js
+++ cypress/e2e/elements/register_elements.js
@@ -1,5 +1,24 @@
 class RegisterElements {
 
-    
+    inputFirstName = () => { return '[placeholder="First Name"' }
+    inputLastName = () => { return '[placeholder="Last Name"]' }
+    inputAdddress = () => { return '//*[@id="basicBootstrapForm"]/div[2]/div/textarea' }
+    inputAddressEmail = () => { return "//input[@type='email']" }
+    inputPhone = () => { return "//input[@type='tel']" }
+    selectFile = () => { return '#imagesrc' }
+    selectGender = () => { return "//input[@type='radio']" }
+    checkBoxHobbies = () => { return "//input[@id='checkbox1']" }
+    selectLanguage = () => { return '#msdd' }
+    selectSkills = () => { return '#Skills' }
+    selectCountry = () => { return '#country' }
+    selectYear = () => { return '#yearbox' }
+    selectMonth = () => { return '[placeholder="Month"]' }
+    selectDay = () => { return '#daybox' }
+    inputPassword = () => { return '#firstpassword' }
+    inputConfirmPassword = () => { return '#secondpassword' }
+    btnSubmit = () => { return '#submitbtn' }
+    btnRefresh = () => { return '#Button1' }
+
+
 
 } export default RegisterElements;
--- cypress/e2e/pages/register_page.js
+++ cypress/e2e/pages/register_page.js
@@ -3,8 +3,54 @@ import RegisterElements from "../elements/register_elements";
 
 const registerElements = new RegisterElements;
 
-class RegisterPage{
+class RegisterPage {
 
-    
+    fillNameAndLastName(name, lastName) {
+        cy.log('FILL NAME AND LAST NAME');
+        cy.get(registerElements.inputFirstName()).type(name);
+        cy.get(registerElements.inputLastName()).type(lastName);
+    }
+    fillContacts(address, email, phone) {
+        cy.log('FILL ADDRESS');
+        cy.contains(registerElements.inputAdddress()).type(address);
+        cy.contains(registerElements.inputAddressEmail()).type(email);
+        cy.contains(registerElements.inputPhone()).type(phone);
+    }
+    selectRadioGender(gender) {
+        cy.log('SELECT GENDER');
+        cy.contains(registerElements.selectGender()).check(gender);
+    }
+    selectCheckHobbies(hobbies) {
+        cy.log('SELECT HOBBIES');
+        cy.contains(registerElements.checkBoxHobbies()).check(hobbies);
+    }
+    selectLanguages() {
+        cy.log('SELECT LANGUAGES');
 
-}export default RegisterPage;
+    }
+    selectSkills(skills) {
+        cy.log('SELECT SKILLS');
+        cy.get(registerElements.selectSkills()).select(skills);
+    }
+    selectCountry(country) {
+        cy.log('SELECT COUNTRY');
+        cy.get(registerElements.selectCountry()).select(country);
+    }
+    selectBirthDay(year, month, day) {
+        cy.log('SELECT BIRTHDAY');
+        cy.get(registerElements.selectYear()).select(year);
+        cy.get(registerElements.selectMonth()).select(month);
+        cy.get(registerElements.selectDay()).select(day);
+    }
+    inputPasswords(pass, confirmPass) {
+        cy.log('INPUT PASSWORD');
+        cy.get(registerElements.inputPassword()).type(pass);
+        cy.get(registerElements.inputConfirmPassword()).type(confirmPass);
+    }
+
+    btnSubmit() {
+        cy.log('BTN SUBMIT');
+        cy.get(registerElements.btnSubmit()).click();
+    }
+
+} export default RegisterPage;
