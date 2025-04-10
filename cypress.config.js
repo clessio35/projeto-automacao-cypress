fe0c692828480b24c7f9b5648ff4a769fedd0285
Clessio <clessio.silva1989@gmail.com>
2025-04-10 08:53:12 -0300

    build

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,22 @@
 const { defineConfig } = require("cypress");
-const cucumber = require("cypress-cucumber-preprocessor").default
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
   e2e: {
-    defaultCommandTimeout: 10000,
     baseUrl: "https://phptravels.net/",
-    specPattern: "**/*.feature",
-    setupNodeEvents(on, config) {
-      on("file:preprocessor", cucumber());
+    defaultCommandTimeout: 10000,
+    specPattern: "cypress/e2e/**/*.feature", // Onde estão seus arquivos .feature
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
     },
   },
 });
-
